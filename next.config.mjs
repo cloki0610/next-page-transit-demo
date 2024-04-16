3ac262d9ae60de39c8b0a998e56c8dd9203965ef
Rocky.C <cloki0610@gmail.com>
2024-04-17 02:41:13 +0800

    Update github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: "/next-page-transit-demo",
+    output: "export",
+    reactStrictMode: true,
+};
 
 export default nextConfig;
