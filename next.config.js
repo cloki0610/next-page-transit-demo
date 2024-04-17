85540094ff81d749a387bc9496abe696c0ae5f91
Rocky.C <cloki0610@gmail.com>
2024-04-18 05:42:22 +0800

    Update config and test new action

--- next.config.js
+++ next.config.js
@@ -4,10 +4,6 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    generateBuildId: async () => {
-    // This could be anything, using the latest git hash
-        return process.env.GIT_HASH
-    },
 };
 
 module.exports = nextConfig;
