8360aa937901d5f311274d4320620526408441b8
Rocky.C <cloki0610@gmail.com>
2024-04-18 05:33:40 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
+    generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+        return process.env.GIT_HASH
+    },
 };
 
 module.exports = nextConfig;
